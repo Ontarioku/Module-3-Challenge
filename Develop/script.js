5c4d828070784db7766bbd181ebc49832f08556f
Ontarioku <148642510+Ontarioku@users.noreply.github.com>
2023-11-16 12:22:51 -0400

    Add files via upload

---
+++ Develop/script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var password = document.getElementById("password");
+var button = document.getElementById("generate")
+ function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ }
+
+function copyPassword() {
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelection(0,999);
+  document.execCommand("copy");
+}
+button.addEventListener("click", genPassword);
+button.addEventListener("click", copyPassword);
